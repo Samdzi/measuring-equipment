8184340f4b43f27da8b04e9b6e739aa7a82e878c
samdzi <samir@dzidic.de>
2018-01-14 10:24:11 +0100

    First real Commit

---
+++ C#/app/Controllers/ToolsController.cs
@@ -0,0 +1,59 @@
+﻿using app.Services;
+using app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web;
+
+namespace app.Controllers
+{
+    public class ToolsController : ApiController
+    {
+        private ToolRepository toolRepository;
+
+        public ToolsController()
+        {
+            this.toolRepository = new ToolRepository();
+        }
+        public Tool[] Get()
+        {
+            return this.toolRepository.GetAllTools();
+        }
+        //public HttpResponseMessage Post(Tool tool)
+        //{
+        //    this.toolRepository.SaveTool(tool);
+
+        //    var response = Request.CreateResponse<Tool>(System.Net.HttpStatusCode.Created, tool);
+
+        //    return response;
+        //}
+
+    }
+    //public class ToolController : Controller
+    //{
+    //    public ActionResult Index()
+    //    {
+    //        ViewBag.Title = "Tools";
+
+    //        return View();
+    //    }
+    //}
+    public class ToolController : Controller
+    {
+        private ToolRepository toolRepository2;
+        public ToolController()
+        {
+            this.toolRepository2 = new ToolRepository();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Tools";
+
+            return View();
+        }
+    }
+}
---
+++ C#/app/Services/ToolRepository.cs
@@ -0,0 +1,86 @@
+﻿using app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace app.Services
+{
+    public class ToolRepository
+    {
+        private Tool[] tools;
+
+        public ToolRepository()
+        {
+            var tools = new Tool[]
+            {
+                new Tool
+                {
+                    Id = 1, Name = "Bügelmessschraube"
+                },
+                new Tool
+                {
+                    Id = 2, Name = "Messschieber"
+                }
+            };
+            this.tools = tools;
+        }
+        public Tool[] GetAllTools()
+        {
+            if (this.tools != null)
+            {
+                return this.tools;
+            }
+
+            return new Tool[]
+            {
+                new Tool
+                {
+                    Id = 0,
+                    Name = "Placeholder"
+                }
+            };
+        }
+        //public Tool[] GetAllTools()
+        //{
+        //    if (this.tools != null)
+        //    {
+        //        return this.tools;
+        //    }
+
+        //    return new Tool[]
+        //    {
+        //        new Tool
+        //        {
+        //            Id = 0,
+        //            Name = "Placeholder"
+        //        }
+        //    };
+        //}
+        //private const string CacheKey = "ToolStore";
+
+        //public bool SaveTool(Tool tool)
+        //{
+        //    var ctx = HttpContext.Current;
+
+        //    if (ctx != null)
+        //    {
+        //        try
+        //        {
+        //            var currentData = ((Tool[])ctx.Cache[CacheKey]).ToList();
+        //            currentData.Add(tool);
+        //            ctx.Cache[CacheKey] = currentData.ToArray();
+
+        //            return true;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.ToString());
+        //            return false;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+    }
+}
